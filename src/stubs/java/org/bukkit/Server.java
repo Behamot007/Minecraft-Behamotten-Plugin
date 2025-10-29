ce201d4740387c04791b26f7136bddfc6395c309
Behamot <33199868+Behamot007@users.noreply.github.com>
2025-10-29 12:22:45 +0100

    Fix Bukkit server stub to match interface

--- src/stubs/java/org/bukkit/Server.java
+++ src/stubs/java/org/bukkit/Server.java
@@ -1,6 +1,5 @@
 package org.bukkit;
 
-import java.util.Collections;
 import java.util.Iterator;
 import org.bukkit.advancement.Advancement;
 import org.bukkit.plugin.PluginManager;
@@ -8,14 +7,8 @@ import org.bukkit.plugin.PluginManager;
 /**
  * Minimal server stub used for compilation.
  */
-public class Server {
-    private final PluginManager pluginManager = new PluginManager();
+public interface Server {
+    PluginManager getPluginManager();
 
-    public PluginManager getPluginManager() {
-        return pluginManager;
-    }
-
-    public Iterator<Advancement> advancementIterator() {
-        return Collections.emptyIterator();
-    }
+    Iterator<Advancement> advancementIterator();
 }
--- src/stubs/java/org/bukkit/plugin/java/JavaPlugin.java
+++ src/stubs/java/org/bukkit/plugin/java/JavaPlugin.java
@@ -1,18 +1,22 @@
 package org.bukkit.plugin.java;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.logging.Logger;
 
 import org.bukkit.Server;
+import org.bukkit.advancement.Advancement;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 
 /**
  * Minimal JavaPlugin stub providing only the methods required for compilation.
  */
 public class JavaPlugin implements Plugin {
     private final Logger logger = Logger.getLogger(getClass().getName());
-    private final Server server = new Server();
+    private final Server server = new MockServer();
 
     public void onEnable() {
         // no-op
@@ -36,5 +40,19 @@ public class JavaPlugin implements Plugin {
 
     public Server getServer() {
         return server;
+    }
+
+    private static final class MockServer implements Server {
+        private final PluginManager pluginManager = new PluginManager();
+
+        @Override
+        public PluginManager getPluginManager() {
+            return pluginManager;
+        }
+
+        @Override
+        public Iterator<Advancement> advancementIterator() {
+            return Collections.emptyIterator();
+        }
     }
 }
